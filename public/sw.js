46e1848480960f92f149fe0438de8d3e0fa424a1
harshedits571 <harshks12345@gmail.com>
2026-03-26 00:10:50 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,39 @@
-const CACHE_NAME = 'ew-os-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/src/style.css',
-  '/src/main.js'
-];
+const CACHE_NAME = 'ew-os-v2';
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  self.skipWaiting(); // Force installation immediately
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Clear old caches!
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// NETWORK-FIRST STRATEGY: Always try to get the newest file from the internet first!
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        // We got a successful response from the live internet, cache the new version!
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => {
+        // If there is NO internet, ONLY THEN load the cached version.
+        return caches.match(event.request);
+      })
   );
 });
